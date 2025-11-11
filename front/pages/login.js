31a4e6453cb304e1d60b96543fde6fb296abf826
Catalin-Cio <thesocialcmc@gmail.com>
2025-11-11 17:35:12 +0200

    inlocuieste placeholder cu poza aleasa

--- front/pages/login.js
+++ front/pages/login.js
@@ -26,22 +26,14 @@ const Page = () => {
           </div>
         </div>
       </div>
-      <div className="hidden flex-1 items-center justify-center border-l border-border-primary bg-sidebar-bg lg:flex">
-        <div className="flex h-64 w-64 items-center justify-center rounded-full bg-gray-200">
-          <svg
-            className="h-32 w-32 text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={1.5}
-              d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-            />
-          </svg>
-        </div>
+      {/* Right side: show a full-cover conference image (fills the entire right panel). */}
+      <div className="hidden lg:flex flex-1 border-l border-border-primary bg-sidebar-bg relative overflow-hidden">
+        {/* Image is served from /images/conference.jpg (place file at front/public/images/conference.jpg) */}
+        <img
+          src="/images/conference.jpg"
+          alt="conference"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
       </div>
     </div>
   );
